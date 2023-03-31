cc003ee9a9d1b5cef90b5807844051e3b1a0ffa5
salsabila juandira <92494551+sjuandira@users.noreply.github.com>
2023-03-31 16:11:46 +0700

    Add files via upload

--- HomeFragment.java
+++ HomeFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TiketFragment#newInstance} factory method to
+ * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TiketFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class TiketFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TiketFragment() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class TiketFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TiketFragment.
+     * @return A new instance of fragment HomeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TiketFragment newInstance(String param1, String param2) {
-        TiketFragment fragment = new TiketFragment();
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class TiketFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tiket, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
--- ProfileFragment.java
+++ ProfileFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TiketFragment#newInstance} factory method to
+ * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TiketFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class TiketFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TiketFragment() {
+    public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class TiketFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TiketFragment.
+     * @return A new instance of fragment ProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TiketFragment newInstance(String param1, String param2) {
-        TiketFragment fragment = new TiketFragment();
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class TiketFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tiket, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
